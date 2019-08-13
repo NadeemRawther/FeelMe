9e16353d6bfb6f1e5546a63e2d2138d8c661eca0
nadeem <nadeemsani786@hotmail.com>
2019-08-13 16:59:26 +0530

    Its my new update

--- app/src/main/java/com/example/feelme/SignUp.java
+++ app/src/main/java/com/example/feelme/SignUp.java
@@ -18,6 +18,23 @@ EditText fath_phone ,mother_phone,grandpa_phone,grandma_phone,uncle_phone,aunt_p
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         fatherimg_btn = (Button)findViewById(R.id.father_img_btn);
+        motherimg_btn = (Button)findViewById(R.id.mother_img_btn);
+        grandpaimg_btn = (Button)findViewById(R.id.grandpa_img_btn);
+        grandmaimg_btn = (Button)findViewById(R.id.grandma_img_btn);
+        uncleimg_btn =(Button)findViewById(R.id.uncle_img_btn);
+        auntimg_btn = (Button)findViewById(R.id.aunt_img_btn);
+        sisterimg_btn = (Button)findViewById(R.id.sister_img_btn);
+        brotherimg_btn = (Button)findViewById(R.id.brotherimg_btn);
+        fatherimg = (ImageView)findViewById(R.id.father_img);
+        motherimg = (ImageView)findViewById(R.id.motherimg);
+        grandpaimg = (ImageView)findViewById(R.id.grandpa_img);
+        grandmaimg = (ImageView)findViewById(R.id.grandma_img);
+        uncleimg= (ImageView)findViewById(R.id.uncle_img);
+        auntimg = (ImageView)findViewById(R.id.aunt_img);
+        sisterimg = (ImageView)findViewById(R.id.sisterimg);
+        brotherimg = (ImageView)findViewById(R.id.brother_img);
+        
+
 
 
 
